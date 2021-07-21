fe8b226bea85fbf144f6363d527ae0327daa7fba
Oliver Saks <oliversaks@Olivers-MBP.fios-router.home>
2021-07-21 19:01:44 -0400

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Running Project Locally
-
-- at the root run `npm i` to install dependencies, then `npm run start`.
-
-# About
-
-This is a very small snippet from a personal project I have been working on, and is based on [A Million Times Clock](https://www.humanssince1982.com/a-million-times). This was created with `create-react-app` and built using node `v12.18.2`.
---
+++ precache-manifest.cfeb7a8fc96341cfcd0928f5775d3875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8de6420d3cd2133ee3c2f077cdd130f",
+    "url": "/sample/index.html"
+  },
+  {
+    "revision": "9f827e2838fea5a74592",
+    "url": "/sample/static/css/main.b75638f9.chunk.css"
+  },
+  {
+    "revision": "0fb1040fd604665be41c",
+    "url": "/sample/static/js/2.f7339832.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/sample/static/js/2.f7339832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f827e2838fea5a74592",
+    "url": "/sample/static/js/main.8e455812.chunk.js"
+  },
+  {
+    "revision": "6180853ef6f426e7630d",
+    "url": "/sample/static/js/runtime-main.928cbe74.js"
+  }
+]);
